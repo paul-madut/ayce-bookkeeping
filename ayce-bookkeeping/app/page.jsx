b4bc0212fb7b2d111db57008718d820c6d73a589
Paul Madut <paul.madut@mths.ca>
2025-05-14 14:25:48 -0400

    metadata 2

--- ayce-bookkeeping/app/page.jsx
+++ ayce-bookkeeping/app/page.jsx
@@ -38,6 +38,11 @@ export const AnimatedText = ({ children, delay = 0, className = "" }) => {
 export default function page() {
   return (
     <>
+    <link rel="icon" href="/favicon.ico" />
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
       <LampContainer className=" pt-[14rem] ">
         <motion.h1
           initial={{ opacity: 0.5, y: 100 }}
