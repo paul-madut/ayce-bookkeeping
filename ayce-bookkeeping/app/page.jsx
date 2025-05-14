65eb89409798ccf66c67432e18de8e5a7f387e9b
Paul Madut <paul.madut@mths.ca>
2025-05-14 14:28:14 -0400

    metadata 3

--- ayce-bookkeeping/app/page.jsx
+++ ayce-bookkeeping/app/page.jsx
@@ -39,8 +39,8 @@ export default function page() {
   return (
     <>
     <link rel="icon" href="/favicon.ico" />
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="/logo.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="/logo.png" />
 <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 
       <LampContainer className=" pt-[14rem] ">
