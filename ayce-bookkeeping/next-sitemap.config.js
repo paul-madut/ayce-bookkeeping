6a908ed0313a3ac440a0c6fc3912d9468f681f8e
Paul Madut <paul.madut@mths.ca>
2025-05-22 20:52:48 -0400

    sitemaps

---
+++ ayce-bookkeeping/next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://anavo-bookkeeping.com', // Replace with your domain
+    generateRobotsTxt: true, // Generate robots.txt
+    // Optional: exclude pages or set priority
+    // exclude: ['/admin'],
+    // changefreq: 'daily',
+  };
+  
